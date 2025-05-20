847058acce6c973e8980cff4b5309c74b16af23a
Fadri <fadriasliarga@gmail.com>
2025-05-20 07:58:30 +0700

    learn data types

---
+++ Basic/dtypes.py
@@ -0,0 +1,29 @@
+a = "Hello Sekai"
+b = 20
+c = 20.5
+d = 1j
+e = ["Miku", "Teto", "Neru"]
+f = ("Miku", "Teto", "Neru")
+g = range(6)
+h = {"Name" : "Fadri", "age" : 21}
+i = {"Miku", "Teto", "Neru"}
+j = frozenset({"Miku", "Teto", "Neru"})
+k = True
+l = bytearray(5)
+m = memoryview(bytes(5))
+n = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
